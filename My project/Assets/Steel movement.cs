737b1591051bf31e8b97d33b2c7d814b4fb996cc
mc-mc-Thomas <thomas.weber@htl-wolfsberg.at>
2025-09-17 15:47:49 +0200

    sec

---
+++ My project/Assets/Steel movement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steelmovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D rigidbody;
+    private Vector2 move 
+
+
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Processing Inputs
+        
+    }
+
+     void FixedUpdate()
+    {
+        //Pysiks Calculations
+        
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+    }
+
+    void Move()
+    { 
+
+    
+    
+    }
+        
+    
+
+
+   }
