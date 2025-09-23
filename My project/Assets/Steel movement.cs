7ad6cfc9363361e2207db4f80ad6c40ddb03a1c2
AldinJasa <137902239+AldinJasa@users.noreply.github.com>
2025-09-23 16:17:27 +0200

    Movement, anfang animation

--- My project/Assets/Steel movement.cs
+++ My project/Assets/Steel movement.cs
@@ -6,38 +6,65 @@ public class Steelmovement : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody2D rigidbody;
-    private Vector2 move;
+    private Vector2 movedirection;
 
 
-  
+    public Sprite frontSprite;
+    public Sprite backSprite;
+    public Sprite rightSprite;
+    public Sprite leftSprite;
+    public SpriteRenderer spriteRenderer;
+
 
     // Update is called once per frame
     void Update()
     {
         //Processing Inputs
-        
+        ProcessInputs();
+        UpdateSpriteDirection();
+
     }
 
-     void FixedUpdate()
+    void FixedUpdate()
     {
         //Pysiks Calculations
-        
+        Move();
     }
 
     void ProcessInputs()
     {
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
+
+        movedirection = new Vector2(moveX, moveY);
     }
 
     void Move()
-    { 
-
-    
-    
+    {
+        rigidbody.velocity = new Vector2(movedirection.x * moveSpeed, movedirection.y * moveSpeed);
     }
-        
-    
 
 
-   }
+    void UpdateSpriteDirection()
+    {
+        if (movedirection.x > 0)
+        {
+            spriteRenderer.sprite = rightSprite;
+        }
+        else if (movedirection.x < 0)
+        {
+            spriteRenderer.sprite = leftSprite;
+        }
+        else if (movedirection.y > 0)
+        {
+            spriteRenderer.sprite = backSprite;
+        }
+        else if (movedirection.y < 0)
+        {
+            spriteRenderer.sprite = frontSprite;
+        }
+    }
+
+
+
+}
