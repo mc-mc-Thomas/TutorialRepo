a5d3242cfdefb752010d030512602fafcb64ed0b
bernhardkrobath <151643522+bernhardkrobath@users.noreply.github.com>
2025-09-30 16:16:00 +0200

    weiter animiert

--- My project/Assets/Steel movement.cs
+++ My project/Assets/Steel movement.cs
@@ -1,70 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Steelmovement : MonoBehaviour
 {
-    public float moveSpeed;
+    public float moveSpeed = 5f;
     public Rigidbody2D rigidbody;
-    private Vector2 movedirection;
+    private Vector2 moveinput;
+    private Animator animator;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
 
 
-    public Sprite frontSprite;
-    public Sprite backSprite;
-    public Sprite rightSprite;
-    public Sprite leftSprite;
-    public SpriteRenderer spriteRenderer;
-
-
-    // Update is called once per frame
     void Update()
     {
-        //Processing Inputs
-        ProcessInputs();
-        UpdateSpriteDirection();
-
+        rigidbody.velocity = moveinput * moveSpeed;
+       
     }
 
-    void FixedUpdate()
+
+
+    public void Move(InputAction.CallbackContext context)
     {
-        //Pysiks Calculations
-        Move();
-    }
-
-    void ProcessInputs()
-    {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
-
-        movedirection = new Vector2(moveX, moveY);
-    }
-
-    void Move()
-    {
-        rigidbody.velocity = new Vector2(movedirection.x * moveSpeed, movedirection.y * moveSpeed);
-    }
-
-
-    void UpdateSpriteDirection()
-    {
-        if (movedirection.x > 0)
+        animator.SetBool("iswalking", true);
+        if (context.canceled)
         {
-            spriteRenderer.sprite = rightSprite;
+            animator.SetBool("iswalking", false);
+            animator.SetFloat("LastInputX", moveinput.x);
+            animator.SetFloat("LastInputY", moveinput.y);
         }
-        else if (movedirection.x < 0)
-        {
-            spriteRenderer.sprite = leftSprite;
-        }
-        else if (movedirection.y > 0)
-        {
-            spriteRenderer.sprite = backSprite;
-        }
-        else if (movedirection.y < 0)
-        {
-            spriteRenderer.sprite = frontSprite;
-        }
-    }
 
+
+         moveinput = context.ReadValue<Vector2>();
+        animator.SetFloat("MoveX", moveinput.x);
+        animator.SetFloat("MoveY", moveinput.y);
+    }
 
 
 }
