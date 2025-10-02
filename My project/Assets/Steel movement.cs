46de21a185c5fb4ea2bd4df52dd041538b506930
420-Leo-420 <leonhard.zlamy@htl-wolfsberg.at>
2025-10-02 15:53:25 +0200

    Movement fertig (Animationen später noch verschönern)

--- My project/Assets/Steel movement.cs
+++ My project/Assets/Steel movement.cs
@@ -25,21 +25,45 @@ public class Steelmovement : MonoBehaviour
 
 
 
+    //public void Move(InputAction.CallbackContext context)
+    //{
+    //    animator.SetBool("iswalking", true);
+    //    if (context.canceled)
+    //    {
+    //        animator.SetBool("iswalking", false);
+    //        animator.SetFloat("LastinputX", moveinput.x);
+    //        animator.SetFloat("LastinputY", moveinput.y);
+    //    }
+
+
+    //     moveinput = context.ReadValue<Vector2>();
+    //    animator.SetFloat("InputX", moveinput.x);
+    //    animator.SetFloat("InputY", moveinput.y);
+    //}
     public void Move(InputAction.CallbackContext context)
     {
-        animator.SetBool("iswalking", true);
+        // Lese Input-Vektor
+        moveinput = context.ReadValue<Vector2>();
+
+        if (context.performed)
+        {
+            animator.SetBool("iswalking", true);
+            animator.SetFloat("InputX", moveinput.x);
+            animator.SetFloat("InputY", moveinput.y);
+
+            // Nur wenn wirklich Bewegung stattfindet, Richtung merken
+            if (moveinput != Vector2.zero)
+            {
+                animator.SetFloat("LastinputX", moveinput.x);
+                animator.SetFloat("LastinputY", moveinput.y);
+            }
+        }
+
         if (context.canceled)
         {
             animator.SetBool("iswalking", false);
-            animator.SetFloat("LastInputX", moveinput.x);
-            animator.SetFloat("LastInputY", moveinput.y);
+            // KEIN Setzen von LastinputX/Y hier, da moveinput == (0,0)
         }
-
-
-         moveinput = context.ReadValue<Vector2>();
-        animator.SetFloat("MoveX", moveinput.x);
-        animator.SetFloat("MoveY", moveinput.y);
     }
-
 
 }
